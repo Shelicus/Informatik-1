84e6de754c1f617e29b7b0b326e6e0e03a7fa817
blackpirat01 <patrick.pira@t-online.de>
2022-04-06 13:08:29 +0200

    Ü3 Patrick bearbeitet

--- src/blatt03/patrick/SchleifenUebung.java
+++ src/blatt03/patrick/SchleifenUebung.java
@@ -1,4 +1,29 @@
 package blatt03.patrick;
 
 public class SchleifenUebung {
+
+    public static void main(String[] args) {
+        float startwert = -4f;
+        float endwert = 4f;
+        float schrittweite = 0.5f;
+        //quadratzahlen(startwert,endwert,schrittweite);
+        trigonomie(startwert, endwert, schrittweite);
+
+    }
+
+    public static void quadratzahlen(float startwert, float endwert, float schrittweite) {
+        System.out.println("Alle Quadratzahlen von -4 ... 4 in 0.5er-Schritten:");
+        for(float i = startwert; i <= endwert; i+= schrittweite){
+            System.out.println("x, x^2: " + i + ", " + (i*i));
+        }
+    }
+
+    public static void trigonomie(float startwert, float endwert, float schrittweite) {
+        System.out.println("Alle Sinus, Cosinus von -4 ... 4 in 0.5er-Schritten:");
+        for(float i = startwert; i <= endwert; i+= schrittweite) {
+
+        }
+
+
+    }
 }
