672a0e50666e36a536da189e60bcde9b01a71c78
world464 <85952172+world464@users.noreply.github.com>
2022-05-01 12:54:19 +0200

    "Update "

--- src/blatt05/johannes/Huhn.java
+++ src/blatt05/johannes/Huhn.java
@@ -6,6 +6,7 @@ package blatt05.johannes;
 public class Huhn {
 	private String name;
 	private Boolean hungrig = true;
+	private static int anzahlHuehner;//automatisch 0
 	
 	/**	Erstellt ein Huhn mit Namen
 	 * @param pName
@@ -14,6 +15,7 @@ public class Huhn {
 	Huhn(String pName){
 		this.name = pName;
 		//this.hungrig = true;
+		Huhn.anzahlHuehner++;
 	}
 	
 	/** Setzt den die Instanzvariable hungrig auf false
@@ -23,12 +25,28 @@ public class Huhn {
 		this.hungrig = false;
 	}
 	
+	/**Checkt ob das Huhn hungrig ist
+	 * @return 
+	 * 		boolean hungrig
+	 */
+	public boolean isHungrig() {
+		return this.hungrig;
+	}
+	
 	/** Gibt den Namen als String von der Instanz zurueck
 	 * @return name
 	 * 		Als String der Name der Instanz
 	 */
 	public String getName() {
 		return this.name;
+	}
+	
+	/** Gibt die Anzahl Huehner zurueck
+	 * @return Huhn.anzahlHuehner
+	 * 		Anzahl der erstellten Huehner
+	 * 	 */
+	public static int getAnzahlHuehner() {
+		return Huhn.anzahlHuehner;
 	}
 	
 
--- src/blatt05/johannes/Raumzuordnung.java
+++ src/blatt05/johannes/Raumzuordnung.java
@@ -4,21 +4,45 @@ package blatt05.johannes;
 public class Raumzuordnung {
 	private Raum raum;		// Referenz auf Raum
 	private Person nutzer;	// Referenz auf Nutzer/in
+	private String grundZuordnung;
 	// TODO Grund für Zuordnung (z.B. "Büroraum", "Labor") als Zeichenkette ergänzen. 
 	//		Darf nicht NULL sein und muss mindestens aus 3 Zeichen bestehen. 
 	//		Bitte auch passende Zugriffsmethode und zusätzlichen Konstruktor 
 	//		mit Zuordnungsgrund hinzufügen.
 	
+	/** Gettermethode*/
+	public String getGrundZuordnung() {
+		return this.grundZuordnung;
+	}
+	
+	/** Konstruktor2 */
+	public Raumzuordnung(Raum raum, Person nutzer, String grundZuordnung) {
+		this(raum, nutzer);
+		final int MINIMALE_ANZAHL_ZEICHEN = 3;
+		if(grundZuordnung == null || grundZuordnung.length() < MINIMALE_ANZAHL_ZEICHEN) {
+				System.err.println("Nullpointerexception: Zuordnungsgrund ist null oder < 3 Zeichen");
+		} else {
+			this.grundZuordnung = grundZuordnung;
+		}
+	}
+		
 	/** Konstruktor */
 	public Raumzuordnung(Raum raum, Person nutzer) {
 		// TODO: prüfen, dass jeweils nicht null
-		this.raum = raum; this.nutzer = nutzer;
-	}
-	
+		if(raum == null | nutzer == null) {
+			System.err.println("Nullpointerexception: Raum oder Nutzer ist null");
+		} else {
+			this.raum = raum; this.nutzer = nutzer;	
+			}
+		} 
+
 	public void print() {
 		this.getNutzer().print();
 		System.out.print(" nutzt den Raum: ");
 		this.getRaum().print();
+		if(this.getGrundZuordnung() != null) {
+			System.out.print(" " + this.getGrundZuordnung());
+		}
 	}
 	
 	Raum getRaum() {
@@ -33,7 +57,8 @@ public class Raumzuordnung {
 		// Bröckl nutzt den Raum E 212a
 		Person ub = new Person("Ulrich", "Bröckl");
 		Raum e212a = new Raum("e 212a", 2);
-		Raumzuordnung ub2e212a = new Raumzuordnung(e212a, ub);
+		String gZ = new String("als Büro");
+		Raumzuordnung ub2e212a = new Raumzuordnung(e212a, ub, gZ);
 		ub2e212a.print();	
 
 		// Bröckl nutzt außerdem den Raum E 201
@@ -48,9 +73,9 @@ public class Raumzuordnung {
 		// Vogelsang nutzt den Raum E 209
 		Raumzuordnung hv2e209 = new Raumzuordnung(
 				new Raum("e 209", 3),
-				new Person("Holger", "Vogelsang"));
+				new Person("Holger", "Vogelsang"),
+				new String("als Büro"));
 		hv2e209.print();
-		
 	}
 
 }
