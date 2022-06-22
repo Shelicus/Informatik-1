ce217a43b3b5e6cd2370ba0135933240d4d64047
David <73255970+Shelicus@users.noreply.github.com>
2022-06-22 17:50:24 +0200

    Bearbeitung der Aufgaben 11 - fertig

--- src/blatt11/david/Statistik.java
+++ src/blatt11/david/Statistik.java
@@ -4,29 +4,38 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+
 /** Klasse, um Mittelwert zu berechnen
  * 
  * @author Ulrich.Broeckl@hs-karlsruhe.de 
  *
  */
-public class Statistik{
- 	private double summe;
+public class Statistik {
+	private double summe;
 
 	protected double n;
-	
+
 	String name;
-	
-	
-	/** Konstruktor 
+
+
+	/**
+	 * Konstruktor
+	 *
 	 * @param name Identifikator/Name für die Statistik
 	 */
 	public Statistik(String name) {
-		this.name = name;
-		this.summe = this.n = 0.;
+		if (name == null) throw new NullPointerException("Name of statistic must not be null.\n");
+		else if (name.length() == 0) throw new NullPointerException("Name of statistic must not be empty.\n");
+		else if (!Character.isLetter(name.charAt(0))) throw new NullPointerException("Name of statistic must start with a letter.\n");
+		else {
+			this.name = name;
+			this.summe = this.n = 0.;
+		}
 	}
-	
-	/** Einen neuen Wert der Statistik hinzufügen
-	 * 
+
+	/**
+	 * Einen neuen Wert der Statistik hinzufügen
+	 *
 	 * @param wert
 	 */
 	public void addWert(double wert) {
@@ -36,31 +45,41 @@ public class Statistik{
 
 	/**
 	 * Durchschnitt berechnen
+	 *
 	 * @return Durchschnitt aller Werte
 	 */
 	double getDurchschnitt() {
-		return this.summe / this.n;
+		if (this.n != 0) {
+			return this.summe / this.n;
+		} else {
+			throw new RuntimeException("No average for an empty statistic available.\n");
+		}
 	}
-	
-	/** Statistik in Zeichenkette ausgeben
-	 * 
+
+
+	/**
+	 * Statistik in Zeichenkette ausgeben
 	 */
 	@Override
 	public String toString() {
-		return String.format("%12s: n==%10.0f, µ==%8f", 
-				this.name, this.n, this.getDurchschnitt() );
-	}
-	
-	
-	/** Statistik in Datei mit dem Namen der Statistik und der Endung ".statistik" ausgeben
-	 * @see <a href="https://stackoverflow.com/questions/2885173/how-do-i-create-a-file-and-write-to-it-in-java">...</a>
-	 */
-	public void writeToFile()  {
-		try {
-			Files.write(Paths.get(this.name + ".statistik"), this.toString().getBytes());
-		} catch (IOException e) {
-			System.out.println(e);
+		if (this.n != 0) {
+		return String.format("%12s: n==%10.0f, µ==%8f",
+				this.name, this.n, this.getDurchschnitt());
+		} else {
+			throw new RuntimeException("Empty statistic cannot be serialized.");
 		}
 	}
+
+
+	/**
+	 * Statistik in Datei mit dem Namen der Statistik und der Endung ".statistik" ausgeben
+	 *
+	 * @see <a href="https://stackoverflow.com/questions/2885173/how-do-i-create-a-file-and-write-to-it-in-java">...</a>
+	 */
+	public void writeToFile() throws IOException {
+			Files.write(Paths.get(this.name + ".statistik"), this.toString().getBytes());
+
+	}
+
 
 }
